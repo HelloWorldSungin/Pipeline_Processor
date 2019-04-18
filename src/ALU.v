e6183ab20550af96157a62d96ad900dc941df958
HelloWorldSungin <sunginkim@ucsb.edu>
2019-04-18 15:26:56 -0700

    Updated ALU and its testbench

--- src/ALU.v
+++ src/ALU.v
@@ -1,22 +1,41 @@
+/*
+  Arithmetic Logic Unit (ALU) module will perform arithmetic and logic operations
+  Inputs:
+    In1: First 32 bits input
+    In2: Second 32 bits input
+    Func: this is the selector that determines which arithmetic operation will be
+          computed with In1 and In2
+  Output:
+    ALUout: the output from the arithmetic operation with In1 and In2
+*/
 
-module ALU (input [31:0] In1, In2, input [3:0] Func, output reg [31:0] ALUout);
-  wire [31:0] BInput;
+`default_nettype none
+
+module ALU (
+  input wire [31:0] In1,
+  input wire [31:0] In2,
+  input wire [3:0] Func,
+  output reg [31:0] ALUout
+  );
+
+  // Declare Wires used in this Module
+  wire [31:0] update_In2;
   wire [31:0] sum;
   wire cout, cin, overflow, zero;
 
-  assign BInput = Func[3] ? ~In2 : In2; //If(Func[3])-> BInput = ~In2, else BInput = In2
-  assign {cout, sum} = Func[3]+ In1 + BInput;
-  
-  always @ * begin
-   case (Func[2:0]) 
-    3'b000 : ALUout <= In1 & BInput ; //AND
-    3'b001 : ALUout <= In1 | BInput ; //OR
-    3'b010 : ALUout <= In1 ^ BInput;  //XOR
-    3'b011 : ALUout <= In1 ~^ BInput; //XNOR
-    3'b100 : ALUout <= sum;           //ADD
-    3'b101 : ALUout <= {31'd0, sum[31]}; //SLT or Zero?
-   endcase
-  end    
-  assign zero = (ALUout == 0) ;
-   
- endmodule
+  // Assigning values to the local wires
+  assign updated_In2 = Func[3] ? ~In2 : In2;
+  assign {cout, sum} = Func[3] + In1 + updated_In2;
+  assign zero = (ALUout == 0);
+
+  // ALU arithmetic computation
+  assign ALUout = (Func[2:0] == 3'b000) ? In1 & updated_In2 :               //AND
+                  (Func[2:0] == 3'b001) ? In1 | updated_In2 :               //OR
+                  (Func[2:0] == 3'b010) ? In1 ^ updated_In2 :               //XOR
+                  (Func[2:0] == 3'b011) ? In1 ~^ updated_In2 :              //XNOR
+                  (Func[2:0] == 3'b100) ? sum :                             //ADD
+                  (Func[2:0] == 3'b101) ? {31'd0, sum[31]} : 32'hXXXXXXXX;  //SLT
+
+endmodule
+
+`default_nettype wire
--- test/ALU_tb.v
+++ test/ALU_tb.v
@@ -1,36 +1,99 @@
+`timescale 1 ns / 1 ns
+`default_nettype none
+
 module ALU_tb();
-reg [31:0] In1 = 31'b0101;
-reg [31:0] In2 = 31'b1010;
-reg clk;
+
+//--Inputs--------------------------
+reg [31:0] In1;
+reg [31:0] In2;
 reg [3:0] Func;
+
+//--Output--------------------------
 wire [31:0] ALUout;
-ALU DUT(In1, In2, Func, ALUout);
 
+// Declare local regs and wires
+reg clk;
+reg donesim;
+reg [31:0] ALUout_expected;
 
-always begin
-  #5 clk = ~clk;
+ALU ALU_inst (
+  .In1    (In1),
+  .In2    (In2),
+  .Func   (Func),
+  .ALUout (ALUout)
+  );
+
+// Simulate clk
+initial begin
+  clk = 0;
+  while(donesim !== 1'b1) begin   // == compare bits (0 or 1), === compare 4 states (0,1,x,z)
+    #5
+    clk = ~clk;
+  end
+  $display("done");
+end
+
+// Checking ALUout against ALUout_expected
+initial begin
+  ALUout_expected = ALUout;
+  while(donesim !== 1'b1) begin
+    @(negedge clk);
+    if (ALUout != ALUout_expected) begin
+      $display("Error for Func: %d, In1: %d, In2: %d", Func, In1, In2);
+      $display("ALUout_expected: %d, but ALU_out: %d", ALUout_expected, ALUout);
+      $stop;
+    end
+  end
 end
 
 initial begin
-  clk <= 0;
-  In1 = 31'b0101; //5
-  In2 = 31'b1010; //10
-  Func = 4'b0000; //AND   ALUout = 0000 = 0
-  #20
-  Func = 4'b0001; //OR    ALUout = 1111 = 15
-  #20
-  In2 = 31'b1011; //11
-  Func = 4'b0010; //XOR   ALUout = 1110 = 14
-  #20
-  Func = 4'b0011; //XNOR  ALUout = 1111 1111 1111 1111 1111 1111 1111 0001 = -15
-  #20
-  Func = 4'b0100; //ADD   ALUout = 1000 = 8
-  #20
-  Func = 4'b1100; //SUB   ALUout = 1111 1111 1111 1111 1111 1111 1111 1010 = -6
-  #20
-  Func = 4'b0101; //SLT   ALUout = 0000 = 0
-  #20
+  donesim = 1'b0;
+  In1 = 32'd0;
+  In2 = 32'd0;
+  Func = 4'hX;
+
+  // AND
+  #10
+  Func = 4'b0000;
+  In1 = 32'd5;
+  In2 = 32'd10;
+  ALUout_expected = 32'd0;
+
+  // OR
+  #10
+  Func = 4'b0001;
+  ALUout_expected = 32'd15;
+
+  // XOR
+  #10
+  Func = 4'b0010;
+  In2 = 32'd11;
+  ALUout_expected = 32'd14
+
+  // XNOR
+  #10
+  Func = 4'b0011;
+  ALUout_expected = 32'hFFFFFFF1;
+
+  // ADD
+  #10
+  Func = 4'b0100
+  ALUout_expected = 32'd8;
+
+  // SUB
+  #10
+  Func = 4'b1100;
+  ALUout_expected = 32'hFFFFFFFA;
+
+  // SLT
+  #10
+  Func = 4'b0101;
+  ALUout_expected= 32'd0;
+
+  donesim = 1'b1;
   $stop;
 end
 
 endmodule
+
+`default_nettype wire

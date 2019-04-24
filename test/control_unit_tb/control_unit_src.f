c9e6ae75568992ec2ea463bcacb66f124ddd2b10
HelloWorldSungin <sunginkim@ucsb.edu>
2019-04-23 21:10:28 -0700

    corrected file type in the name

--- test/control_unit_tb/control_unit_src.f
+++ test/control_unit_tb/control_unit_src.f
@@ -1,3 +1,3 @@
-../../src/control_unit/ALU_decoder
-../../src/control_unit/main_decoder
-../../src/control_unit/control_unit_TOP
+../../src/control_unit/ALU_decoder.v
+../../src/control_unit/main_decoder.v
+../../src/control_unit/control_unit_TOP.v

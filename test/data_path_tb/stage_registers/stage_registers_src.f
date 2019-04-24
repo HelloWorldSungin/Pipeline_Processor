5c75d4eb90a5206d7ec5989c328dd5debc52c29f
HelloWorldSungin <sunginkim@ucsb.edu>
2019-04-24 06:26:49 -0700

    redirected stage_registers

---
+++ test/data_path_tb/stage_registers/stage_registers_src.f
@@ -0,0 +1,5 @@
+../../../src/data_path/stage_registers/fetch_register.v
+../../../src/data_path/stage_registers/decode_register.v
+../../../src/data_path/stage_registers/execute_register.v
+../../../src/data_path/stage_registers/memory_register.v
+../../../src/data_path/stage_registers/writeback_register.v
--- test/stage_registers/stage_registers_src.f
+++
@@ -1,5 +0,0 @@
-../../src/stage_registers/fetch_register.v
-../../src/stage_registers/decode_register.v
-../../src/stage_registers/execute_register.v
-../../src/stage_registers/memory_register.v
-../../src/stage_registers/writeback_register.v
